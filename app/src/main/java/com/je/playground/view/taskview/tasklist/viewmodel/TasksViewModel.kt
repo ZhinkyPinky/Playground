b02e2eb59c501e3ab615488683a591b741ddcdc2
ZhinkyPinky <jonathan.engstrom@gmail.com>
2024-01-10 18:46:01 +0100

    Simplified TasksViewModel

--- app/src/main/java/com/je/playground/view/taskview/tasklist/viewmodel/TasksViewModel.kt
+++ app/src/main/java/com/je/playground/view/taskview/tasklist/viewmodel/TasksViewModel.kt
@@ -5,20 +5,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.je.playground.database.tasks.entity.MainTask
 import com.je.playground.database.tasks.entity.MainTaskWithSubTasks
-import com.je.playground.database.tasks.entity.SubTask
 import com.je.playground.database.tasks.repository.TasksRepository
 import com.je.playground.notification.NotificationScheduler
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 data class TasksUiState(
-    val subTasks : List<SubTask> = emptyList(),
-    val mainTasks : List<MainTask> = emptyList(),
     val mainTasksWithSubTasks : List<MainTaskWithSubTasks> = emptyList(),
 )
 
@@ -48,22 +43,10 @@ class TasksViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
-            combine(
-                tasksRepository.getAllSubTasks(),
-                tasksRepository.getAllMainTasks(),
-                tasksRepository.getAllActiveMainTasksWithSubTasks()
-            ) { tasks, taskGroups, taskGroupsWithSubTasks ->
-                TasksUiState(
-                    subTasks = tasks,
-                    mainTasks = taskGroups,
-                    mainTasksWithSubTasks = taskGroupsWithSubTasks
-                )
-            }
-                .catch { throwable ->
-                    throwable.printStackTrace()
-                }
+            tasksRepository
+                .getAllActiveMainTasksWithSubTasks()
                 .collect {
-                    _tasksUiState.value = it
+                    _tasksUiState.value = TasksUiState(it)
                 }
         }
 
